8211547cd6fd903e1fe0235e3671f1202272e105
Andres Patino <andrespa87@gmail.com>
2022-07-07 13:42:10 -0500

    Fixing loaded time

--- src/main/java/com/mycompany/testautomationsequal/TestAutomationSEQUAL.java
+++ src/main/java/com/mycompany/testautomationsequal/TestAutomationSEQUAL.java
@@ -33,7 +33,7 @@ public class TestAutomationSEQUAL {
         //Crea Objeto tipo WebDriver
         WebDriver driver = new ChromeDriver(options);
 
-        //Define 10 segundos de espera maximo para encontrar los elementos
+        //Define 15 segundos de espera maximo para encontrar los elementos
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
         //Accede a la pagina web
@@ -44,16 +44,10 @@ public class TestAutomationSEQUAL {
 
         //Espera 2 segundos      
         Thread.sleep(2000);
-        
+
         //Click en ingresar con email
         driver.findElement(By.xpath("(//div[contains(@class,'vtex-button__label flex')])[3]")).click();
 
-//        //Espera 2 segundos      
-//        try {
-//            Thread.sleep(2000);
-//        } catch (InterruptedException ex) {
-//            Logger.getLogger(TestAutomationSEQUAL.class.getName()).log(Level.SEVERE, null, ex);
-//        }
         //Ingreso email
         driver.findElement(By.xpath("(//input[contains(@class,'vtex-styleguide-9-x-input ma0')])[2]")).sendKeys("andrespa87@gmail.com");
 
@@ -64,38 +58,17 @@ public class TestAutomationSEQUAL {
         driver.findElement(By.xpath("(//div[contains(@class,'vtex-button__label flex')])[3]")).click();
 
         //Espera 4 segundos      
-        try {
-            Thread.sleep(4000);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(TestAutomationSEQUAL.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Thread.sleep(4000);
+
         //buscar palabra "atun"
         driver.findElement(By.xpath("//input[contains(@class,'vtex-styleguide-9-x-input ma0')]")).sendKeys("atun");
 
-//        //Espera 3 segundos      
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException ex) {
-//            Logger.getLogger(TestAutomationSEQUAL.class.getName()).log(Level.SEVERE, null, ex);
-//        }
         //click en ver todos los resultados
         driver.findElement(By.className("vtex-search-2-x-tileListSeeMore")).click();
 
-//        //Espera 6 segundos      
-//        try {
-//            Thread.sleep(6000);
-//        } catch (InterruptedException ex) {
-//            Logger.getLogger(TestAutomationSEQUAL.class.getName()).log(Level.SEVERE, null, ex);
-//        }
         //Click en seleccionar ciudad
         driver.findElement(By.xpath("//div[contains(@class,'MuiInputBase-input MuiInput-input')]//div")).click();
 
-//        //Espera 1 segundos      
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException ex) {
-//            Logger.getLogger(TestAutomationSEQUAL.class.getName()).log(Level.SEVERE, null, ex);
-//        }
         //Seleccionar ciudad medellin
         driver.findElement(By.xpath("//*[@id=\"react-select-2-input\"]")).sendKeys("medellin");
         driver.findElement(By.xpath("//*[@id=\"react-select-2-input\"]")).sendKeys(Keys.RETURN);
@@ -104,59 +77,30 @@ public class TestAutomationSEQUAL {
         driver.findElement(By.xpath("//button[@class='exito-geolocation-3-x-primaryButton shippingaddress-confirmar']")).click();
 
         //Espera 10 segundos     
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(TestAutomationSEQUAL.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Thread.sleep(15000);
 
         //click atun ekono  
         driver.findElement(By.xpath("//span[text()='EKONO']/following::div[@class='exito-vtex-components-4-x-blockBuyButton']")).click();
 
-//        //Espera 4 segundos      
-//        try {
-//            Thread.sleep(4000);
-//        } catch (InterruptedException ex) {
-//            Logger.getLogger(TestAutomationSEQUAL.class.getName()).log(Level.SEVERE, null, ex);
-//        }
         //Click boton envio a domicilio
         driver.findElement(By.xpath("(//button[contains(@class,'exito-geolocation-3-x-orderOptionsButton ')])[2]")).click();
 
-//        //Espera 10 segundos     
-//        try {
-//            Thread.sleep(10000);
-//        } catch (InterruptedException ex) {
-//            Logger.getLogger(TestAutomationSEQUAL.class.getName()).log(Level.SEVERE, null, ex);
-//        }
         //click confirmar direccion
         driver.findElement(By.xpath("//button[@class='exito-geolocation-3-x-primaryButton addressbyuser-confirmar']")).click();
 
         //Espera 5 segundos      
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(TestAutomationSEQUAL.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Thread.sleep(5000);
 
         //click en carrito de compras
         driver.findElement(By.xpath("(//a[contains(@class,'exito-header-3-x-minicartLink')])")).click();
 
-//        //Espera 5 segundos      
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException ex) {
-//            Logger.getLogger(TestAutomationSEQUAL.class.getName()).log(Level.SEVERE, null, ex);
-//        }
         //click en ir a pagar
         driver.findElement(By.xpath("(//a[contains(@class,'btn btn-large btn-success pull-left-margin btn-place-order')])")).click();
 
         //Espera 10 segundos
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(TestAutomationSEQUAL.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Thread.sleep(10000);
 
+        
         //Cierra el navegador
         driver.close();
 
